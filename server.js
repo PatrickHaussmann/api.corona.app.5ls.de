a877f68661aecf17813049de1d3d223e99201622
Patrick Haußmann <36867676+PatrickHaussmann@users.noreply.github.com>
2020-11-03 10:27:59 +0100

    Update and rename landkreise.js to server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-import axios from 'axios';
+const axios = require("axios");
 
-export default async (req, res) => {
+module.exports = async(req, res) => {
 
     res.setHeader('Cache-Control', 's-maxage=3600');
 
