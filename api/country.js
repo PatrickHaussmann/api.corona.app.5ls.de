7d1b2b02673b82358f67f620c64ebe9dea1559f1
Patrick Haußmann <36867676+PatrickHaussmann@users.noreply.github.com>
2020-11-17 17:37:58 +0100

    Update country.js

--- api/country.js
+++ api/country.js
@@ -1,4 +1,6 @@
 const axios = require("axios");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
 module.exports = async (req, res) => {
 
@@ -6,8 +8,10 @@ module.exports = async (req, res) => {
 
     const response = await axios.get("https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Fallzahlen.html");
     
-    var htmlDoc = document.createElement( 'html' );
-    htmlDoc.innerHTML = response.data
+    const dom = new JSDOM(response.data);
+    var htmlDoc = dom.window.document
+    
+    result.lastUpdate = htmlDoc.getElementById("main").getElementsByTagName("p")[0].innerText.split(" (")[0].substring(7)
     
     let tds = htmlDoc.getElementById("main").getElementsByTagName("table")[0].getElementsByTagName("tbody")[0].getElementsByTagName("tr")[16].getElementsByTagName("td")
     
@@ -16,7 +20,6 @@ module.exports = async (req, res) => {
     result.weekIncidence = tds[4].innertText
     result.diff = tds[2].innertText
     result.last7d = tds[3].innertText
-    
 
     res.json(result)
 }
