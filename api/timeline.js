00bbe9c1d8c1283e911849a2d212d36915437e81
PatrickHaussmann <36867676+PatrickHaussmann@users.noreply.github.com>
2020-11-21 17:44:38 +0100

    timeline api

---
+++ api/timeline.js
@@ -0,0 +1,32 @@
+const axios = require("axios");
+
+module.exports = async (req, res) => {
+
+    let result = {}
+    result.timeline = []
+
+    const response = await axios.get("https://api.covid19api.com/dayone/country/germany");
+    const apidata = response.data;
+
+    result.country = apidata[0].Country
+    result.country_code = apidata[0].CountryCode
+    result.lat = apidata[0].Lat
+    result.lon = apidata[0].Lon
+
+    let confirmed = 0
+    for (const element of apidata) {
+        let simplified = {}
+        simplified.Date = element.Date;
+        simplified.confirmed = element.Confirmed;
+        simplified.deaths = element.Deaths;
+        simplified.recovered = element.Recovered;
+        simplified.active = element.Active;
+        simplified.diff = simplified.confirmed - confirmed;
+
+        confirmed = simplified.confirmed;
+
+        result.timeline.push(simplified);
+    }
+
+    res.json(result)
+}
