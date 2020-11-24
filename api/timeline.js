366d855031bd3bf4aa0471a4022ed7cc5fd5a1a8
PatrickHaussmann <36867676+PatrickHaussmann@users.noreply.github.com>
2020-11-24 22:43:54 +0100

    timeline data direct from github/JHU

--- api/timeline.js
+++ api/timeline.js
@@ -1,32 +1,73 @@
 const axios = require("axios");
+const parse = require('csv-parse/lib/sync')
 
 module.exports = async (req, res) => {
 
-    let result = {}
-    result.timeline = []
+    const confirmed_promise = axios.get("https://cdn.jsdelivr.net/gh/CSSEGISandData/COVID-19@latest/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv");
+    const recovered_promise = axios.get("https://cdn.jsdelivr.net/gh/CSSEGISandData/COVID-19@latest/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv");
+    const deaths_promise = axios.get("https://cdn.jsdelivr.net/gh/CSSEGISandData/COVID-19@latest/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv");
 
-    const response = await axios.get("https://api.covid19api.com/dayone/country/germany");
-    const apidata = response.data;
+    const [confirmed_response, recovered_response, deaths_response] = await Promise.all([confirmed_promise, recovered_promise, deaths_promise]);
 
-    result.country = apidata[0].Country
-    result.country_code = apidata[0].CountryCode
-    result.lat = apidata[0].Lat
-    result.lon = apidata[0].Lon
+    let options = { columns: true, skip_empty_lines: true }
 
-    let confirmed = 0
-    for (const element of apidata) {
-        let simplified = {}
-        simplified.date = element.Date;
-        simplified.confirmed = element.Confirmed;
-        simplified.deaths = element.Deaths;
-        simplified.recovered = element.Recovered;
-        simplified.active = element.Active;
-        simplified.diff = simplified.confirmed - confirmed;
+    const confirmed_apidata = parse(confirmed_response.data, options)
+    const recovered_apidata = parse(recovered_response.data, options)
+    const deaths_apidata = parse(deaths_response.data, options)
 
-        confirmed = simplified.confirmed;
+    var confirmed, recovered, deaths
 
-        result.timeline.push(simplified);
+    function filterGermany(row) {
+        if (row["Country/Region"] == "Germany") return true
+        return false
     }
 
-    res.json(result)
+    var confirmed = confirmed_apidata.filter(filterGermany)[0];
+    var recovered = recovered_apidata.filter(filterGermany)[0];
+    var deaths = deaths_apidata.filter(filterGermany)[0];
+
+    var lat = Number(confirmed.Lat), lon = Number(confirmed.Long)
+
+    var timeline = []
+    var dates = {}
+
+    for (const date in confirmed) {
+        if (confirmed.hasOwnProperty(date) && !["Province/State", "Country/Region", "Lat", "Long"].includes(date)) {
+            dates[date] = { date: new Date(date).toLocaleString('de-DE', { day: "2-digit", month: "2-digit", year: "numeric" }) }
+            dates[date].confirmed = Number(confirmed[date])
+        }
+    }
+
+    for (const date in recovered) {
+        if (recovered.hasOwnProperty(date) && !["Province/State", "Country/Region", "Lat", "Long"].includes(date)) {
+            dates[date].recovered = Number(recovered[date])
+        }
+    }
+
+    for (const date in deaths) {
+        if (deaths.hasOwnProperty(date) && !["Province/State", "Country/Region", "Lat", "Long"].includes(date)) {
+            dates[date].deaths = Number(deaths[date])
+        }
+    }
+
+    let previous_confirmed = 0
+    for (const date in dates) {
+        if (dates.hasOwnProperty(date)) {
+            const element = dates[date];
+            element.active = element.confirmed - element.recovered - element.deaths
+            element.diff = element.confirmed - previous_confirmed;
+
+            timeline.push(element)
+            previous_confirmed = element.confirmed;
+        }
+    }
+
+    res.json({
+        timeline,
+        "country": "Germany",
+        "country_code": "DE",
+        lat,
+        lon
+    })
 }
+
