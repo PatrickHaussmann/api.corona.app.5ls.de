35aa60ed9fa3407e8249c334bcdc56fa1fe9e618
Patrick Haußmann <36867676+PatrickHaussmann@users.noreply.github.com>
2020-11-27 22:15:08 +0100

    lower case in api response

--- api/districts.geojson.js
+++ api/districts.geojson.js
@@ -11,9 +11,11 @@ module.exports = async (req, res) => {
     simplified.crs = undefined
 
     for (const feature of simplified.features) {
+        feature.properties.ags = feature.properties.AGS
+        feature.properties.gen = feature.properties.GEN
         for (const key in feature.properties) {
             if (feature.properties.hasOwnProperty(key)) {
-                if (!["GEN", "AGS"].includes(key)) feature.properties[key] = undefined
+                if (!["gen", "ags"].includes(key)) feature.properties[key] = undefined
             }
         }
     }
