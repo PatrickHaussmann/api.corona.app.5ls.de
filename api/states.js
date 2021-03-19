dcfa4964558fdfcb11ce328a507e4f1679dbaae0
patrickhaussmann <git@patrickhaussmann.de>
2021-03-19 15:32:17 +0100

    v2: add states endpoint

--- api/states.js
+++ api/states.js
@@ -1,73 +1,11 @@
 const axios = require("axios");
 
 module.exports = async (req, res) => {
+    const response = await axios.get("https://api.corona-zahlen.org/states");
+    let result = response.data;
 
-    let states = [];
-    let total = {
-        count: 0,
-        deaths: 0,
-        weekIncidence: 0,
-        casesPer100k: 0
-    }
-
-    const response = await axios.get("https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/Coronaf%C3%A4lle_in_den_Bundesl%C3%A4ndern/FeatureServer/0/query?where=1%3D1&outFields=LAN_ew_GEN,LAN_ew_EWZ,Fallzahl,Aktualisierung,faelle_100000_EW,Death,cases7_bl_per_100k&returnGeometry=false&outSR=4326&f=json");
-    const apidata = response.data;
-
-    for (const feature of apidata.features) {
-        let state = {}
-        state.name = feature.attributes.LAN_ew_GEN;
-        state.count = feature.attributes.Fallzahl;
-        state.deaths = feature.attributes.Death;
-        state.weekIncidence = feature.attributes.cases7_bl_per_100k;
-        state.casesPer100k = feature.attributes.faelle_100000_EW;
-        state.code = getAbbreviation(state.name);
-
-        states.push(state);
-
-        total.count += state.count
-        total.deaths += state.deaths
-        total.weekIncidence += state.weekIncidence
-        total.casesPer100k += state.casesPer100k
-    }
-
-    res.json({ lastUpdate: apidata.features[0].attributes.Aktualisierung, total: total ,states: states })
-}
-
-function getAbbreviation(name) {
-    switch (name) {
-        case "Baden-Württemberg":
-            return "BW";
-        case "Bayern":
-            return "BY";
-        case "Berlin":
-            return "BE";
-        case "Brandenburg":
-            return "BB";
-        case "Bremen":
-            return "HB";
-        case "Hamburg":
-            return "HH";
-        case "Hessen":
-            return "HE";
-        case "Mecklenburg-Vorpommern":
-            return "MV";
-        case "Niedersachsen":
-            return "NI";
-        case "Nordrhein-Westfalen":
-            return "NW";
-        case "Rheinland-Pfalz":
-            return "RP";
-        case "Saarland":
-            return "SL";
-        case "Sachsen":
-            return "SN";
-        case "Sachsen-Anhalt":
-            return "ST";
-        case "Schleswig-Holstein":
-            return "SH";
-        case "Thüringen":
-            return "TH";
-        default:
-            return "Unknown";
-    }
-}
+    result.lastUpdate = result.meta.lastUpdate;
+    result.lastCheckedForUpdate = new Date();
+    result.meta = undefined;
+    res.json(result);
+};
