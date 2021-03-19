795d08e003171d79b0535b84a247e3ef37b74bf3
patrickhaussmann <git@patrickhaussmann.de>
2021-03-19 15:31:16 +0100

    v2: updated districs

--- api/districts.js
+++ api/districts.js
@@ -1,165 +1,47 @@
 const axios = require("axios");
-const interpolate = require('color-interpolate');
-const rgbaToHex = require('hex-and-rgba').rgbaToHex;
-const parse = require('color-parse')
-const lerp = require('lerp')
-
-function map(n, start1, stop1, start2, stop2) {
-    return ((n - start1) / (stop1 - start1)) * (stop2 - start2) + start2;
-};
-
-function toHex(color) {
-    let parsed_color = parse(color)
-    let rgb = parsed_color.values
-
-    if (parsed_color.alpha != 1) rgb.push(parsed_color.alpha)
-    let hex = rgbaToHex(...rgb)
-    return hex
-}
-
 
 module.exports = async (req, res) => {
+    const cases_promise = axios.get("https://api.corona-zahlen.org/districts");
+    const beds_promise = axios.get(
+        "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/DIVI_Intensivregister_Landkreise/FeatureServer/0/query?where=1%3D1&outFields=AGS,betten_frei,betten_belegt,betten_gesamt,Anteil_betten_frei,faelle_covid_aktuell,faelle_covid_aktuell_beatmet,Anteil_covid_beatmet,Anteil_COVID_betten,daten_stand&returnGeometry=false&outSR=4326&f=json"
+    );
 
-    let data = {};
-    let series = {};
+    const [cases_response, beds_response] = await Promise.all([
+        cases_promise,
+        beds_promise,
+    ]);
 
-    const cases_promise = axios.get("https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?where=1%3D1&outFields=AGS,RS,GEN,EWZ,cases,deaths,cases_per_100k,cases_per_population,last_update,cases7_per_100k,death_rate&returnGeometry=false&outSR=4326&f=json");
-    const beds_promise = axios.get("https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/DIVI_Intensivregister_Landkreise/FeatureServer/0/query?where=1%3D1&outFields=AGS,betten_frei,betten_belegt,betten_gesamt,Anteil_betten_frei,faelle_covid_aktuell,faelle_covid_aktuell_beatmet,Anteil_covid_beatmet,Anteil_COVID_betten,daten_stand&returnGeometry=false&outSR=4326&f=json");
+    const cases = cases_response.data.data;
+    const beds = beds_response.data;
 
-    const [cases_response, beds_response] = await Promise.all([cases_promise, beds_promise]);
+    let result = {
+        data: cases,
+    };
 
-    const cases_apidata = cases_response.data;
-    const beds_apidata = beds_response.data;
+    for (const feature of beds.features) {
+        let ags = feature.attributes.AGS;
+        if (!result.data[ags]) result.data[ags] = {};
+        let district = result.data[ags];
 
+        district.bedsAvailable = feature.attributes.betten_frei;
+        district.bedsOccupied = feature.attributes.betten_belegt;
+        district.bedsTotal = feature.attributes.betten_gesamt;
+        district.bedsCovid = feature.attributes.faelle_covid_aktuell;
+        district.bedsCovidVentilated =
+            feature.attributes.faelle_covid_aktuell_beatmet;
+        district.proportionBedsAvailable = null;
+        if (district.bedsAvailable != null && district.bedsTotal != null)
+            district.proportionBedsAvailable =
+                (district.bedsAvailable / district.bedsTotal) * 100;
 
-    function update_series(keys, dict) {
-        keys.forEach(key => {
-            if (key in series) {
-                series[key].min = Math.min(series[key].min, dict[key])
-                series[key].max = Math.max(series[key].max, dict[key])
-            } else {
-                series[key] = {
-                    min: dict[key],
-                    max: dict[key]
-                }
-            }
-        })
+        district.proportionBedsCovid = feature.attributes.Anteil_COVID_betten;
+        district.proportionBedsCovidVentilated =
+            feature.attributes.Anteil_covid_beatmet;
     }
 
-
-    for (const feature of cases_apidata.features) {
-        let rs = feature.attributes.RS
-        data[rs] = {}
-        let district = data[rs]
-
-        district.rs = feature.attributes.RS;
-        district.ags = feature.attributes.AGS;
-        district.name = feature.attributes.GEN;
-        district.cases = feature.attributes.cases;
-        district.deaths = feature.attributes.deaths;
-        district.population = feature.attributes.EWZ;
-        district.week_incidence = feature.attributes.cases7_per_100k;
-        //district.cases_per_100k = feature.attributes.cases_per_100k;
-        district.cases_rate = feature.attributes.cases_per_population;
-        district.death_rate = feature.attributes.death_rate;
-
-        update_series(["cases", "deaths", "population", "week_incidence", "cases_rate", "death_rate"], district)
-    }
-
-
-
-    for (const feature of beds_apidata.features) {
-        let ags = feature.attributes.AGS
-        if (!data[ags]) data[ags] = {}
-        let district = data[ags]
-
-        district.beds_available = feature.attributes.betten_frei;
-        district.beds_occupied = feature.attributes.betten_belegt;
-        district.beds_total = feature.attributes.betten_gesamt;
-        district.beds_covid = feature.attributes.faelle_covid_aktuell;
-        district.beds_covid_ventilated = feature.attributes.faelle_covid_aktuell_beatmet;
-        district.proportion_beds_available = null
-        if (district.beds_available != null && district.beds_total != null) district.proportion_beds_available = district.beds_available / district.beds_total * 100;
-
-        district.proportion_beds_covid = feature.attributes.Anteil_COVID_betten;
-        district.proportion_beds_covid_ventilated = feature.attributes.Anteil_covid_beatmet;
-
-        if (district.proportion_beds_available != null) district.proportion_beds_available = Math.round(district.proportion_beds_available)
-        if (district.proportion_beds_covid != null) district.proportion_beds_covid = Math.round(district.proportion_beds_covid)
-        if (district.proportion_beds_covid_ventilated != null) district.proportion_beds_covid_ventilated = Math.round(district.proportion_beds_covid_ventilated)
-
-        update_series(["beds_available", "beds_occupied", "beds_total", "beds_covid", "beds_covid_ventilated", "proportion_beds_available", "proportion_beds_covid", "proportion_beds_covid_ventilated"], district)
-    }
-
-    
-    series["week_incidence"].name = "Inzidenz"
-    
-    series["week_incidence"].color = [{
-        hex: "#a0a0a0",
-        range_end: 0
-    },{
-        hex: "#d2cd84",
-        range_start: 0,
-        range_end: 35
-    },{
-        hex: "#c6941a",
-        range_start: 35,
-        range_end: 50
-    },{
-        hex: "#bb5220",
-        range_start: 50,
-        range_end: 100
-    },{
-        hex: "#af2632",
-        range_start: 100,
-        range_end: 200
-    },{
-        hex: "#8c0619",
-        range_start: 200
-    }]
-
-    for (const key in series) {
-        if (series.hasOwnProperty(key)) {
-            const element = series[key];
-            if (!element.color) {
-                element.color = [{
-                    hex: "#a0a0a0",
-                    range_end: 0
-                }]
-
-                let palette = ["#69d2e7", "#a7dbd8", "#e0e4cc", "#f38630", "#fa6900"]
-                //let palette = ["#000", "#fff"]
-                let colormap = interpolate(palette);
-                let n = 5
-                let min = 0
-                let max = element.max
-
-                let previous_start = 0
-
-                for (let i = 0; i < n; i++) {
-                    let color = colormap(map(i, 0, n - 1, 0, 1))
-
-                    let range_end = Math.floor(lerp(min, max, map(i + 1, 0, n, 0, 1)))
-                    if (i == n - 1) range_end = undefined
-                    let range_start = previous_start
-                    previous_start = range_end
-
-                    let hex = toHex(color)
-                    element.color.push({ hex, range_start, range_end })
-                }
-            }
-        }
-    }
-
-
-    let districts = [];
-    for (const key in data) {
-        if (data.hasOwnProperty(key)) {
-            const element = data[key];
-            districts.push(element)
-        }
-    }
-
-    res.json({ series, districts, last_update: cases_apidata.features[0].attributes.last_update })
-
-}
+    result.lastUpdate = cases_response.data.meta.lastUpdate;
+    result.lastUpdateBeds = cases_response.data.meta.lastUpdate;
+    result.lastCheckedForUpdate = new Date();
+    result.meta = undefined;
+    res.json(result);
+};
