c070b3bb5652fcf4f7c971b736b1e59cbaccadd9
patrickhaussmann <git@patrickhaussmann.de>
2021-03-20 13:00:47 +0100

    add vaccinations to states

--- api/states.js
+++ api/states.js
@@ -1,11 +1,38 @@
 const axios = require("axios");
 
 module.exports = async (req, res) => {
-    const response = await axios.get("https://api.corona-zahlen.org/states");
-    let result = response.data;
+    const cases_promise = axios.get("https://api.corona-zahlen.org/states");
+    const vaccinations_promise = axios.get(
+        "https://api.corona-zahlen.org/vaccinations"
+    );
 
-    result.lastUpdate = result.meta.lastUpdate;
+    const [cases_response, vaccinations_response] = await Promise.all([
+        cases_promise,
+        vaccinations_promise,
+    ]);
+
+    const cases = cases_response.data.data;
+    const vaccinations = vaccinations_response.data.data.states;
+
+    let states = cases;
+
+    for (const state in vaccinations) {
+        if (
+            Object.hasOwnProperty.call(vaccinations, state) &&
+            state != "Bund"
+        ) {
+            const element = vaccinations[state];
+            if (!states[state]) states[state] = {};
+            element.name = undefined;
+            states[state].vaccination = element;
+        }
+    }
+
+    let result = {
+        states,
+    };
+
+    result.lastUpdate = cases_response.data.meta.lastUpdate;
     result.lastCheckedForUpdate = new Date();
-    result.meta = undefined;
     res.json(result);
 };
