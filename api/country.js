d753b42ad0a75738469278bfcacef8f7507d38b5
patrickhaussmann <git@patrickhaussmann.de>
2021-03-22 09:18:42 +0100

    country: add deathRate and casesRate

--- api/country.js
+++ api/country.js
@@ -1,5 +1,7 @@
 const axios = require("axios");
 
+const population = 83783945;
+
 module.exports = async (req, res) => {
     const cases_promise = axios.get("https://api.corona-zahlen.org/germany");
     const vaccinations_promise = axios.get(
@@ -16,13 +18,18 @@ module.exports = async (req, res) => {
     let result = cases;
     result.rValue = result.r.value;
     result.r = undefined;
-    
-    result.vaccinated = vaccinations.vaccinated
-    result.delta.vaccinated = vaccinations.delta
-    result.vaccinatedQuote = vaccinations.quote
-    result.secondVaccination = vaccinations.secondVaccination.vaccinated
-    result.delta.secondVaccination = vaccinations.secondVaccination.delta
-    result.secondVaccinationQuote = vaccinations.secondVaccination.quote
+
+    if (population != null && result.cases != null)
+        result.casesRate = result.cases / population;
+    if (result.cases != null && result.deaths != null)
+        result.deathRate = result.deaths / result.cases;
+
+    result.vaccinated = vaccinations.vaccinated;
+    result.delta.vaccinated = vaccinations.delta;
+    result.vaccinatedQuote = vaccinations.quote;
+    result.secondVaccination = vaccinations.secondVaccination.vaccinated;
+    result.delta.secondVaccination = vaccinations.secondVaccination.delta;
+    result.secondVaccinationQuote = vaccinations.secondVaccination.quote;
     result.lastUpdateVaccinations = vaccinations_response.data.meta.lastUpdate;
 
     result.lastUpdate = result.meta.lastUpdate;
