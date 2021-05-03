d3f86b1fb438f73e9a42ebffdc461ce87c608813
Patrick Haußmann <git@patrickhaussmann.de>
2021-05-03 20:58:20 +0200

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -3,5 +3,5 @@ const axios = require("axios");
 module.exports = async (req, res) => {
   const response = await axios.get("https://api.corona-zahlen.org/states");
   
-  res.json({response, req, res});
+  res.json({response, query: req.query, url: req.url});
 };
